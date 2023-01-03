b77d581f4844ba5b479b9d4ff4974b2d9d9a46cb
F.T <fthufuss@alpine>
2023-01-03 17:34:33 +0000

    add config.fish

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+
+    # aliases
+    alias a='ansible'
+    alias ap='ansible-playbook'
+
+    function pyenv
+	source ~/pyenv/bin/activate.fish
+    end
+
+end
